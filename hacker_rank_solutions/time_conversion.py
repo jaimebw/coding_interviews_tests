eeb8b5512eb62400018bca9a58164e7616bce167
Jaime Bowen <jaimebwv@gmail.com>
2022-10-25 13:03:23 -0400

    New exercise

---
+++ hacker_rank_solutions/time_conversion.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: Jaime Bowen Varela
+def timeConversion(s):
+    # Write your code here
+    if s[-2:] == "PM":
+        if s[0:2] == "12":
+            pass
+        else:
+            s = s.replace(s[0:2], str(int(s[0:2])+12))
+        s = s.replace("PM","")
+    else:
+        s = s.replace("AM","")
+        if s[0:2] == "12":
+            s = s.replace(s[0:2], "00")
+    return s
+
+test = "07:05:45PM"
+output = "19:05:45"
+
+if output == timeConversion(test):
+    print("Good")
